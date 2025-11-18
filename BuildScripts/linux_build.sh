e3134a0df4ef3f1a47e50099e094e55dc85fb7be
zhouyue <269018784@qq.com>
2025-11-18 11:29:33 +0800

    linux build

---
+++ BuildScripts/linux_build.sh
@@ -0,0 +1,6 @@
+cmake -S .. -B ../../AEngine_build -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=Release -DUSE_QT6=true  -DCMAKE_PREFIX_PATH="/opt/Qt6/6.10.0/gcc_64/lib/cmake"
+cmake --open   ../../AEngine_build
+
+
+
+
