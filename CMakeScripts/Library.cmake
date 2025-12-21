1b6b5fd800463e51f031f01079d222fe0ff36791
zhouyue <269018784@qq.com>
2025-12-21 17:49:45 +0800

    cmake fdkaac

--- CMakeScripts/Library.cmake
+++ CMakeScripts/Library.cmake
@@ -76,7 +76,10 @@ endif ()
 endif ()
 
 if (${USE_FDKAAC})
-	list (APPEND INCLUDE_PATH   ${LIBRARY_PATH}/fdk-aac/2.0.3/include)
+	list (APPEND INCLUDE_PATH   ${LIBRARY_PATH}/fdk-aac/2.0.3/include
+								${LIBRARY_PATH}/fdk-aac/2.0.3/fdk-aac
+	)
+
 	list (APPEND LINK_PATH      ${LIBRARY_PATH}/fdk-aac/2.0.3/lib)
 	list (APPEND LIBRARYS		fdk-aac)
 endif ()
