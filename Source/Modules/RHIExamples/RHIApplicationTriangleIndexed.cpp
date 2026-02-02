f4cdd7bd24eba30a59c34d4aa5ab170f3cd8ae2e
Your Name <you@example.com>
2026-02-03 02:28:04 +0800

    RHIApplicationTriangleIndexed

--- Source/Modules/RHIExamples/RHIApplicationTriangleIndexed.cpp
+++ Source/Modules/RHIExamples/RHIApplicationTriangleIndexed.cpp
@@ -17,7 +17,7 @@ static float VertexAttributes[] = {
      0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f,        0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
     -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f,       -0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
     -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f,       -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-};
+};·
 #else
 
 static float VertexAttributes[] = {
