383a54b6cf822761d7e338d9c7404f0c7434503b
zhouyue <269018784@qq.com>
2026-03-15 17:19:40 +0800

    SSD1306

--- Source/Modules/DeviceDriver/SSD1306.cpp
+++ Source/Modules/DeviceDriver/SSD1306.cpp
@@ -5,11 +5,47 @@ void SSD1306::WriteCommand(uint8_t Cmd)
 
 }
 
+void SSD1306::WriteCommand(uint8_t Cmd, uint8_t Data)
+{
+
+}
+
+void SSD1306::Initialize()
+{
+    TurnOff();
+    OLED_WRITE_COMMAND(0x00);
+    OLED_WRITE_COMMAND(0x10);
+    SetDisplayStartLine(0x0);
+    /*
+     * 亮度拉满
+     */
+    SetContrastControl(0xff);
+    EnableSegmentRemap();
+    EnableCOMRemap();
+    SetMultiplexRatio(64);
+    SetDisplayOffset(0x00);
+    SetDisplayClockDivideRatio_SetOscillatorFrequency(0x80);
+    SetPrechargePeriod(0xf1);
+    SetCOMPinsHardwareConfiguration(0x12);
+    SetVCOMHDeselectLevel(0x40);
+    SetPageAddressingMode();
+    EnableChargePump();
+
+    /*
+    * must disabled
+    */
+    DisableEntireDisplay();// Disable Entire Display On (0xa4/0xa5)
+    DisableInverse();// Disable Inverse Display On (0xa6/a7)
+    TurnOn();
+}
+
 void SSD1306::SetPage(uint8_t Page)
 {
     uint8_t Cmd = 0xb0 + Page;
     WriteCommand(Cmd);
 }
+
+
 
 void SSD1306::SetPageAddress(uint8_t Start, uint8_t End)
 {
@@ -83,6 +119,49 @@ void SSD1306::SetDisplayOffset(uint8_t Offset)
     WriteCommand(Offset);
 }
 
+void SSD1306::SetContrastControl(uint8_t Data)
+{
+    uint8_t Cmd = 0x81;
+    WriteCommand(Cmd);
+    WriteCommand(Data);
+}
+
+void SSD1306::SetMultiplexRatio(uint8_t Ratio)
+{
+    uint8_t Cmd = 0xa8;
+    Ratio -= 0x1;
+    WriteCommand(Cmd);
+    WriteCommand(Ratio);
+}
+
+void SSD1306::SetDisplayClockDivideRatio_SetOscillatorFrequency(uint8_t Data)
+{
+    uint8_t Cmd = 0xd5;
+    WriteCommand(Cmd);
+    WriteCommand(Data);
+}
+
+void SSD1306::SetPrechargePeriod(uint8_t Data)
+{
+    uint8_t Cmd = 0xd9;
+    WriteCommand(Cmd);
+    WriteCommand(Data);
+}
+
+void SSD1306::SetVCOMHDeselectLevel(uint8_t Data)
+{
+    uint8_t Cmd = 0xdb;
+    WriteCommand(Cmd);
+    WriteCommand(Data);
+}
+
+void SSD1306::SetCOMPinsHardwareConfiguration(uint8_t Data)
+{
+    uint8_t Cmd = 0xda;
+    WriteCommand(Cmd);
+    WriteCommand(Data);
+}
+
 void SSD1306::EnableInverse()
 {
     uint8_t Cmd = 0xa7;
@@ -135,6 +214,19 @@ void SSD1306::DisableChargePump()
     WriteCommand(Cmd);
     WriteCommand(Data);
 }
+
+void SSD1306::EnableEntireDisplay()
+{
+    uint8_t Cmd = 0xa5;
+    WriteCommand(Cmd);
+}
+
+void SSD1306::DisableEntireDisplay()
+{
+    uint8_t Cmd = 0xa4;
+    WriteCommand(Cmd);
+}
+
 
 void SSD1306::TurnOn()
 {
--- Source/Modules/DeviceDriver/SSD1306.h
+++ Source/Modules/DeviceDriver/SSD1306.h
@@ -8,7 +8,10 @@
 class SSD1306
 {
 public:
+    void Initialize();
     void WriteCommand(uint8_t Cmd);
+    void WriteCommand(uint8_t Cmd, uint8_t Data);
+
     void SetPage(uint8_t Page);
     void SetPageAddress(uint8_t Start, uint8_t End);
     void SetColumnAddress(uint8_t Start, uint8_t End);
@@ -24,6 +27,21 @@ public:
      */
     void SetDisplayStartLine(uint8_t Line);
     void SetDisplayOffset(uint8_t Offset);
+    void SetContrastControl(uint8_t Data);
+    /*
+     * Ratio: [16, 64]
+     */
+    void SetMultiplexRatio(uint8_t Ratio);
+    /*
+     * 控制帧率
+     */
+    void SetDisplayClockDivideRatio_SetOscillatorFrequency(uint8_t Data);
+
+    void SetPrechargePeriod(uint8_t Data);
+
+    void SetVCOMHDeselectLevel(uint8_t Data);
+
+    void SetCOMPinsHardwareConfiguration(uint8_t Data);
     void EnableInverse();
     void DisableInverse();
     void EnableCOMRemap();
@@ -32,6 +50,8 @@ public:
     void DisableSegmentRemap();
     void EnableChargePump();
     void DisableChargePump();
+    void EnableEntireDisplay();
+    void DisableEntireDisplay();
     void TurnOn();
     void TurnOff();
 
