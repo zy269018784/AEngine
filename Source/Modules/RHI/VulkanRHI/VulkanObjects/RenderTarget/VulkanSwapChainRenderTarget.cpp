4c9f41168e735c36c541d5916d45aca6edc00be2
zhouyue <269018784@qq.com>
2026-04-14 17:22:12 +0800

    render target

--- Source/Modules/RHI/VulkanRHI/VulkanObjects/RenderTarget/VulkanSwapChainRenderTarget.cpp
+++ Source/Modules/RHI/VulkanRHI/VulkanObjects/RenderTarget/VulkanSwapChainRenderTarget.cpp
@@ -79,131 +79,6 @@ VulkanSwapChainRenderTarget::~VulkanSwapChainRenderTarget()
 
 }
 
-
-/*
-
-    std::vector<VkCommandBuffer> commandBuffers;			// 3 == swapChainImages.size()
-	std::vector<VkFramebuffer> swapChainFramebuffers;		// 3 == swapChainImages.size()
-    std::vector<VkSemaphore> imageAvailableSemaphores;		// 2
-    std::vector<VkSemaphore> renderFinishedSemaphores;		// 2
-    std::vector<VkFence> inFlightFences;					// 2
-    std::vector<VkFence> imagesInFlight;					// 3 == swapChainImages.size()
-    size_t currentFrame = 0; [0, 1]
-	imageIndex: [0, 2]
-
-	imagesInFlight[0] = NULL
-	imagesInFlight[1] = NULL
-	imagesInFlight[2] = NULL
-	inFlightFences[0] signaled
-	inFlightFences[1] signaled
-
-Frame 0:
-	currentFrame 	= 0
-	imageIndex		= 0
-	wait inFlightFences[0] 变为signaled
-	imagesInFlight[0] == NULL
-	imagesInFlight[0] = inFlightFences[0];
-
-	reset inFlightFences[0] 变为unsignaled
-	vkQueueSubmit 提交到commandBuffers[0], inFlightFences[0] 变为signaled (提交frame 0)
-
-	imagesInFlight[0] = inFlightFences[0];	image 0
-	imagesInFlight[1] = NULL
-	imagesInFlight[2] = NULL
-	inFlightFences[0] unsignaled			frame 0
-	inFlightFences[1] signaled
-
-Frame 1:
-	currentFrame 	= 1
-	imageIndex		= 1
-	wait inFlightFences[1]变为signaled
-	imagesInFlight[1] == NULL
-	imagesInFlight[1] = inFlightFences[1];
-
-	commandBuffers[0]
-
-	reset inFlightFences[1] 变为unsignaled
-	vkQueueSubmit 提交到commandBuffers[1], inFlightFences[1] 变为signaled (提交frame 1)
-
-	imagesInFlight[0] = inFlightFences[0];
-	imagesInFlight[1] = inFlightFences[1];	image 1
-	imagesInFlight[2] = NULL
-	inFlightFences[0] unsignaled
-	inFlightFences[1] unsignaled 			frame 1
-
-Frame 2:
-	currentFrame 	= 0
-	imageIndex		= 2
-	wait inFlightFences[0]变为signaled (frame 0渲染完)
-	imagesInFlight[2] == NULL
-	imagesInFlight[2] = inFlightFences[0];
-
-	reset inFlightFences[0] 变为unsignaled
-	vkQueueSubmit 提交到commandBuffers[2], inFlightFences[0] 变为signaled (提交frame 2)
-
-	imagesInFlight[0] = inFlightFences[0];
-	imagesInFlight[1] = inFlightFences[1];
-	imagesInFlight[2] = inFlightFences[0];	image 2
-	inFlightFences[0] unsignaled		frame 2
-	inFlightFences[1] unsignaled
-
-Frame 3:
-	currentFrame 	= 1
-	imageIndex		= 0
-	wait inFlightFences[1]变为signaled (frame 1渲染完)
-	vkAcquireNextImageKHR (frame 0呈现完)
-	imagesInFlight[0] != NULL
-		wait imagesInFlight[0]变为signaled  (frame 2渲染完)
-	imagesInFlight[0] = inFlightFences[1];
-
-	reset inFlightFences[1] 变为unsignaled
-	vkQueueSubmit 提交到commandBuffers[0], inFlightFences[1] 变为signaled	(提交frame 3)
-
-	imagesInFlight[0] = inFlightFences[1];	image 0
-	imagesInFlight[1] = inFlightFences[1];
-	imagesInFlight[2] = inFlightFences[0];
-	inFlightFences[0] signaled
-	inFlightFences[1] unsignaled		frame 3
-
-Frame 4:
-	currentFrame 	= 0
-	imageIndex		= 1
-	wait inFlightFences[0]变为signaled
-	imagesInFlight[1] != NULL
-		wait imagesInFlight[1]变为signaled (frame 3渲染完)
-	imagesInFlight[1] = inFlightFences[0];
-
-	reset inFlightFences[0] 变为unsignaled
-	vkQueueSubmit 提交到commandBuffers[1], inFlightFences[0] 变为signaled	(提交frame 4)
-
-	imagesInFlight[0] = inFlightFences[1];
-	imagesInFlight[1] = inFlightFences[0];	image 1
-	imagesInFlight[2] = inFlightFences[0];
-	inFlightFences[0] unsignaled
-	inFlightFences[1] signaled
-
-Frame 5:
-	currentFrame 	= 1
-	imageIndex		= 2
-	wait inFlightFences[1]变为signaled (frame 3渲染完)
-	imagesInFlight[1] != NULL
-		wait imagesInFlight[2]变为signaled (frame 4渲染完)
-	imagesInFlight[2] = inFlightFences[1];
-
-	reset inFlightFences[0] 变为unsignaled
-	vkQueueSubmit 提交到commandBuffers[2], inFlightFences[1] 变为signaled	(提交frame 4)
-
-	imagesInFlight[0] = inFlightFences[1];
-	imagesInFlight[1] = inFlightFences[1];
-	imagesInFlight[2] = inFlightFences[0];
-	inFlightFences[0] signaled
-	inFlightFences[1] unsignaled
-7:
-
-8:
-
-*/
-
 /*
 	3个image:
 		计算中
@@ -227,18 +102,18 @@ void VulkanSwapChainRenderTarget::RHIBeginFrame()
 	//std::cout << "Frame Index " << FrameIndex << " " << Frames.size() << std::endl;
     VulkanFrame* Frame = Frames[FrameIndex];
     /*
-        等待fence变为signaled(RHIEndFrame中QueueSubmit把该fence变为signaled状态)
+		等待QueueSubmit把Frames[FrameIndex]->ImageFence变为signaled状态)
+		Frames[FrameIndex]->ImageFence：   			 		signaled   ->  unsignaled
     */
     Frame->ImageFence->Wait();
 	/*
-	 *  fence变为unsignaled
+	 *  Frames[FrameIndex]->ImageFence：   			 		signaled   ->  unsignaled
 	 */
     Frame->ImageFence->Reset();
-    //VkSemaphore SwapchainImageAvailableSemaphore = frameRes[LastImageIndex].SwapchainImageAvailableSemaphore->GetHandle();
     VkSemaphore SwapchainImageAvailableSemaphore = Frame->ImageAvailableSemaphore->GetHandle();
     /*
         acquire next image
-        SwapchainImageAvailableSemaphore变为Signaled状态
+		Frames[FrameIndex]->ImageAvailableSemaphore: 		unsignaled ->  signaled
     */
     if (SwapChain->AcquireNextImageKHR(UINT64_MAX, SwapchainImageAvailableSemaphore, VK_NULL_HANDLE, &CurrentImageIndex) != VK_SUCCESS)
     {
@@ -293,11 +168,10 @@ void VulkanSwapChainRenderTarget::RHIEndFrame()
 	VkPipelineStageFlags WaitStages[] = { VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT };
 
 	/*
-		提交到队列, signal fence
-		等待SwapchainImageAvailableSemaphore变为Signaled状态
-		command buffer执行完后, 将GraphicsPipelineCompleteSemaphore变为Signaled状态
-	*/
-	//std::cout << " Device->Queues[0] " << Device->Queues[0]->GetHandle() << std::endl;
+		Frames[FrameIndex]->ImageAvailableSemaphore: 		signaled   ->  unsignaled
+		Frames[FrameIndex]->ImageDrawFinishedSemaphore:		unsignaled ->  signaled
+		Frames[FrameIndex]->ImageFence：   			 		unsignaled ->  signaled
+	*/;
 	VkSubmitInfo SubmitInfo{};
 	SubmitInfo.sType				= VK_STRUCTURE_TYPE_SUBMIT_INFO;
 	SubmitInfo.waitSemaphoreCount	= 1;
@@ -317,7 +191,7 @@ void VulkanSwapChainRenderTarget::RHIEndFrame()
 	VkSwapchainKHR SwapChains[] = { SwapChain->GetHandle() };
 
 	/*
-		等待GraphicsPipelineCompleteSemaphore变为Signaled状态
+		Frames[FrameIndex]->ImageDrawFinishedSemaphore:		signaled    ->  unsignaled
 	*/
 	VkPresentInfoKHR PresentInfo{};
 	PresentInfo.sType				= VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
