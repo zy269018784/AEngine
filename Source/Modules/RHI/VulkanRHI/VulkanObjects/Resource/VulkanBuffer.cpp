1ed0d6594d10c65241b93151da6cdf426bc00a38
zhouyue <269018784@qq.com>
2026-04-26 20:05:55 +0800

    VulkanBuffer

--- Source/Modules/RHI/VulkanRHI/VulkanObjects/Resource/VulkanBuffer.cpp
+++ Source/Modules/RHI/VulkanRHI/VulkanObjects/Resource/VulkanBuffer.cpp
@@ -13,42 +13,13 @@ VulkanBuffer::VulkanBuffer(VulkanDevice* InDevice, RHIBufferType InType, RHIBuff
     : Device(InDevice), RHIBuffer(InType, InUsage, InSize)
 {
     DeviceMemoryHandle = new VulkanDeviceMemory(InDevice);
-
-    VkBufferUsageFlags UsageFlags = 0x0;
-#if 0
-    switch (InType)
-    {
-    case VertexBuffer:
-        UsageFlags |= VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
-        break;
-    case IndexBuffer:
-        UsageFlags |= VK_BUFFER_USAGE_INDEX_BUFFER_BIT;
-        break;
-    case UniformBuffer:
-        UsageFlags |= VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
-        break;
-    case StorageBuffer:
-        UsageFlags |= VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
-        break;
-    case IndirectBuffer:
-        UsageFlags |= VK_BUFFER_USAGE_INDIRECT_BUFFER_BIT;
-        break;
-    case TransferSrcBuffer:
-        UsageFlags |= VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
-        break;
-    case TransferDstBuffer:
-        UsageFlags |= VK_BUFFER_USAGE_TRANSFER_DST_BIT;
-        break;
-    }
-#endif
-   UsageFlags = ToVkBufferUsageFlags(InType);
+    
    VkResult Result = VK_SUCCESS;
    
-   VkBufferCreateInfo CreateInfo;
-   std::memset(&CreateInfo, 0, sizeof(CreateInfo));
-   CreateInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-   CreateInfo.size = InSize;
-   CreateInfo.usage = UsageFlags;
+   VkBufferCreateInfo CreateInfo{};
+   CreateInfo.sType     = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+   CreateInfo.size      = InSize;
+   CreateInfo.usage     = ToVkBufferUsageFlags(InType);
    Result = CreateBuffer(&CreateInfo, nullptr);
    if (VK_SUCCESS != Result)
        std::cout << "CreateBuffer failed : " << Result << std::endl;
