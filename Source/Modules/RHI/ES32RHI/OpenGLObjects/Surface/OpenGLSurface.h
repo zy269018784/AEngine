9d20f16cc4ac7cd83e7b193939a55dc0ef78c7c7
zhouyue <269018784@qq.com>
2026-05-10 18:16:28 +0800

    OpenGLSurface

--- Source/Modules/RHI/ES32RHI/OpenGLObjects/Surface/OpenGLSurface.h
+++ Source/Modules/RHI/ES32RHI/OpenGLObjects/Surface/OpenGLSurface.h
@@ -1,7 +1,20 @@
 #pragma once
 #include "RHI/RHIObjects/Surface/RHISurface.h"
-#if RHI_USE_WIN32_KHR
-	#include <windows.h>
+#if OS_IS_WINDOWS
+	#include <windows.h>               // 主头文件
+	#include <windef.h>                // 基本类型定义
+	#include <minwindef.h>
+#endif
+
+#if  OS_IS_ANDROID
+
+#endif
+
+#if  OS_IS_LINUX
+	#include <xcb/xcb.h>
+	#include <X11/Xlib-xcb.h>
+	#include <X11/Xutil.h>
+	#include <X11/Xlib.h>
 #endif
 class OpenGLInstance;
 class OpenGLSurface : public RHISurface
