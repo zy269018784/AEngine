d9cd91ceab75ef64242b3b720672c836300d4260
zhouyue <269018784@qq.com>
2026-05-10 17:22:36 +0800

    windows export

--- Source/Modules/Window/WindowExport.h
+++ Source/Modules/Window/WindowExport.h
@@ -1,9 +1,18 @@
 #pragma once
-
+#if 0
 #if defined(_WIN32) || defined(_WIN64)
     #define WINDOWEXPORT __declspec(dllexport)
     #define WINDOWIMPORT __declspec(dllimport)
 #elif defined(__linux__)
+    #define WINDOWEXPORT __attribute__((visibility("default")))
+    #define WINDOWIMPORT
+#endif
+#endif
+
+#if OS_IS_WINDOWS
+    #define WINDOWEXPORT __declspec(dllexport)
+    #define WINDOWIMPORT __declspec(dllimport)
+#elif OS_IS_LINUX
     #define WINDOWEXPORT __attribute__((visibility("default")))
     #define WINDOWIMPORT
 #endif
