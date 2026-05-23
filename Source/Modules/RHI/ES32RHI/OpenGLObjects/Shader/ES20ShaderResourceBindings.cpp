905f923ce12ef7bf8817f4df74d101bccedb4a2b
zhouyue <269018784@qq.com>
2026-05-24 02:15:59 +0800

    es20

--- Source/Modules/RHI/ES32RHI/OpenGLObjects/Shader/ES20ShaderResourceBindings.cpp
+++ Source/Modules/RHI/ES32RHI/OpenGLObjects/Shader/ES20ShaderResourceBindings.cpp
@@ -32,26 +32,32 @@ void ES20ShaderResourceBindings::CreateSSBO(int BindingPoint,OpenGLBuffer *Buffe
 
 void ES20ShaderResourceBindings::CreateCombinedImageSampler(int TextureUnit, OpenGLTexture *Texture, OpenGLSampler *Sampler)
 {
+	std::cout << "ES20ShaderResourceBindings::CreateCombinedImageSampler 4" << std::endl;
 	auto TextureHandle = Texture->GetHandle();
 	auto SamplerHandle = Sampler->GetHandle();
+	std::cout << "ES20ShaderResourceBindings::CreateCombinedImageSampler 5" << std::endl;
 	auto AddressModeU = ToOpenGLSamplerAddressMode(Sampler->GetAddressModeU());
 	auto AddressModeV = ToOpenGLSamplerAddressMode(Sampler->GetAddressModeV());
 	auto AddressModeW = ToOpenGLSamplerAddressMode(Sampler->GetAddressModeW());
 	auto MinFilter = ToOpenGLFilter(Sampler->GetMinFilter());
 	auto MagFilter = ToOpenGLFilter(Sampler->GetMagFilter());
+	std::cout << "ES20ShaderResourceBindings::CreateCombinedImageSampler 6" << std::endl;
 	/*
 		不可以超过最大纹理单元
 	*/
 	if (TextureUnit < MaxTextureUnits)
 	{
 		// 要求es 2.0以上	都支持
+		std::cout << "ES20ShaderResourceBindings::CreateCombinedImageSampler 1" << std::endl;
 		glActiveTexture(GL_TEXTURE0 + TextureUnit);
 		glBindTexture(GL_TEXTURE_2D, TextureHandle);
 
+		std::cout << "ES20ShaderResourceBindings::CreateCombinedImageSampler 2" << std::endl;
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, MinFilter);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, MagFilter);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, AddressModeU);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, AddressModeV);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, AddressModeW);
+		std::cout << "ES20ShaderResourceBindings::CreateCombinedImageSampler 3" << std::endl;
 	}
 }
