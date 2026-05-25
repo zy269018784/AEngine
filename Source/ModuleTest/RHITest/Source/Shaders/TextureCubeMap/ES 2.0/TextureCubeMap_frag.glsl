2cb1959194b239dfe24991b790c77c5e37b842b0
zhouyue <269018784@qq.com>
2026-05-25 18:51:54 +0800

    cubemap es 2.0

--- Source/ModuleTest/RHITest/Source/Shaders/TextureCubeMap/ES 2.0/TextureCubeMap_frag.glsl
+++ Source/ModuleTest/RHITest/Source/Shaders/TextureCubeMap/ES 2.0/TextureCubeMap_frag.glsl
@@ -5,14 +5,14 @@ precision highp float;
 uniform samplerCube cubemap;
 
 // varying 变量（对应之前的 in）
-varying vec2 inTexCoord;
+varying vec2 outTexCoord;
 
 void main()
 {
     // 构造立方体纹理采样方向
-    // 原代码: vec3(-1, inTexCoord)
-    // inTexCoord.x 和 inTexCoord.y 提供另外两个坐标
-    vec3 direction = vec3(-1.0, inTexCoord.x, inTexCoord.y);
+    // 原代码: vec3(-1, outTexCoord)
+    // outTexCoord.x 和 outTexCoord.y 提供另外两个坐标
+    vec3 direction = vec3(-1.0, outTexCoord.x, outTexCoord.y);
     
     // ES 2.0 使用 textureCube 函数采样立方体贴图
     vec4 color = textureCube(cubemap, direction);
