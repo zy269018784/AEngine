01623d19d507d51f21e59d0f4a6f172b3d872609
zhouyue <269018784@qq.com>
2026-06-03 06:52:11 +0800

    vulkan examples

--- Source/ModuleTest/RHITest/Source/Vulkan/Core/Core.cpp
+++ Source/ModuleTest/RHITest/Source/Vulkan/Core/Core.cpp
@@ -1,10 +1,39 @@
-#include "Core.h"
+﻿#include "Core.h"
 VkInstance Instance;
 std::vector<VkPhysicalDevice> PhysicalDevices;
+std::vector<VkQueueFamilyProperties> QueueFamilyProperties;
 void VulkanInit()
 {
     Instance = CreateInstance();
     std::cout << "VkInstance " << Instance << std::endl;
 
     PhysicalDevices = EnumeratePhysicalDevices();
+
+    CreateQueue();
+}
+
+
+std::vector<VkPhysicalDevice> EnumeratePhysicalDevices()
+{
+    /*
+        获取物理设备数量
+    */
+    std::uint32_t PhysicalDeviceCount = 0;
+    vkEnumeratePhysicalDevices(Instance, &PhysicalDeviceCount, nullptr);
+
+    /*
+        获取物理设备Handle
+    */
+    std::vector<VkPhysicalDevice> PhysicalDeviceHandles;
+    PhysicalDeviceHandles.resize(PhysicalDeviceCount);
+    vkEnumeratePhysicalDevices(Instance, &PhysicalDeviceCount, PhysicalDeviceHandles.data());
+    return PhysicalDeviceHandles;
+}
+
+void CreateQueue()
+{
+    std::uint32_t Count = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(PhysicalDevices[0], &Count, nullptr);
+    QueueFamilyProperties.resize(Count);
+    vkGetPhysicalDeviceQueueFamilyProperties(PhysicalDevices[0], &Count, QueueFamilyProperties.data());
 }
--- Source/ModuleTest/RHITest/Source/Vulkan/Core/Core.h
+++ Source/ModuleTest/RHITest/Source/Vulkan/Core/Core.h
@@ -1,12 +1,16 @@
-#pragma once
+﻿#pragma once
 #include <vulkan/vulkan.h>
 #include <vector>
 #include <iostream>
+void VulkanInit();
 VkInstance CreateInstance();
 std::vector<VkPhysicalDevice> EnumeratePhysicalDevices();
-void VulkanInit();
+void CreateQueue();
+void CreateDevices();
 
 extern VkInstance Instance;
 extern std::vector<VkPhysicalDevice> PhysicalDevices;
+extern std::vector<VkQueueFamilyProperties> QueueFamilyProperties;
+extern std::vector<VkDevice> Devices;
 
 
