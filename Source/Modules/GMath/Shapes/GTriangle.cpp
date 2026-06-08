39372d91d0cfa7e9da1067800a46b0a2c0652b2f
zy269018784 <269018784@qq.com>
2026-06-08 09:08:48 +0800

    UV

--- Source/Modules/GMath/Shapes/GTriangle.cpp
+++ Source/Modules/GMath/Shapes/GTriangle.cpp
@@ -8,13 +8,34 @@ namespace GMath
 
     }
 
-    GTriangle::GTriangle(const Point3f* p0, const Point3f* p1, const Point3f* p2, const Point2f* uv0, const Point2f* uv1, const Point2f* uv2, GAbstractShape *Parent)
-        : GAbstractShape(Parent), p0(*p0), p1(*p1), p2(*p2), uv0(*uv0), uv1(*uv1), uv2(*uv2)
+    GTriangle::GTriangle(const Point3f* p0, const Point3f* p1, const Point3f* p2,
+                         const Point2f* uv0, const Point2f* uv1, const Point2f* uv2,
+                         GAbstractShape *Parent)
+        : GAbstractShape(Parent), p0(*p0), p1(*p1), p2(*p2) , uv0(Point2f(0, 0)), uv1(Point2f(1, 0)), uv2(Point2f(1, 1))
     {
-        dp02 = this->p0 - this->p2;
-        dp12 = this->p1 - this->p2;
+        if (uv0)
+            this->uv0 = *uv0;
+
+        if (uv1)
+            this->uv1 = *uv1;
+
+        if (uv2)
+            this->uv2 = *uv2;
+
+        this->dp02 = this->p0 - this->p2;
+        this->dp12 = this->p1 - this->p2;
         // 着色法线 = 几何法线
-        s_n = g_n = Normal3f(Normalize(Cross(dp02, dp12)));
+        this->s_n = this->g_n = Normal3f(Normalize(Cross(dp02, dp12)));
+
+        std::cout << "dp02 = (" << dp02.x << ", " << dp02.y << ", " << dp02.z << ")" << std::endl;
+        std::cout << "dp12 = (" << dp12.x << ", " << dp12.y << ", " << dp12.z << ")" << std::endl;
+
+        std::cout << "g_n = (" << g_n.x << ", " << g_n.y << ", " << g_n.z << ")" << std::endl;
+        std::cout << "s_n = (" << s_n.x << ", " << s_n.y << ", " << s_n.z << ")" << std::endl;
+
+        std::cout << "uv0 = (" << this->uv0.x << ", " << this->uv0.y << ")" << std::endl;
+        std::cout << "uv1 = (" << this->uv1.x << ", " << this->uv1.y << ")" << std::endl;
+        std::cout << "uv2 = (" << this->uv2.x << ", " << this->uv2.y << ")" << std::endl;
     }
 
     GTriangle::~ GTriangle()
