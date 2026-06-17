eca7e7acbb3e20e78c1de818576dbfeb5d41280a
zhouyue <269018784@qq.com>
2026-06-17 13:07:46 +0800

    RHIBeginRenderPass2

--- Source/Modules/GRHI/VulkanRHI/VulkanObjects/RenderTarget/VulkanSwapChainRenderTarget.cpp
+++ Source/Modules/GRHI/VulkanRHI/VulkanObjects/RenderTarget/VulkanSwapChainRenderTarget.cpp
@@ -185,8 +185,6 @@ void VulkanSwapChainRenderTarget::RHIEndFrame()
 
 void VulkanSwapChainRenderTarget::RHIBeginRenderPass()
 {
-	RHIBeginRenderPass2();
-	return;
     VkClearValue ClearColor[2];
     ClearColor[0].color = {{0.0f, 0.0f, 0.0f, 1.0f}};
     ClearColor[1].depthStencil = {1.0f, 0};  // 深度清除为1.0（最远值
