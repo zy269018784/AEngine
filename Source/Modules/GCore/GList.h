857c2004051bf5cfcaf55801c2d1fee9c0763b9f
zhouyue <269018784@qq.com>
2026-06-21 18:40:54 +0800

    GList

--- Source/Modules/GCore/GList.h
+++ Source/Modules/GCore/GList.h
@@ -13,8 +13,8 @@ private:
         Node* next;
         Node* prev;
 
-        Node(const T& value) : data(value), next(nullptr), prev(nullptr) {}
-        Node(T&& value) : data(std::move(value)), next(nullptr), prev(nullptr) {}
+        Node(const T& Value) : data(Value), next(nullptr), prev(nullptr) {}
+        Node(T&& Value) : data(std::move(Value)), next(nullptr), prev(nullptr) {}
     };
 
     Node* Head;
@@ -33,18 +33,18 @@ public:
         using pointer = T*;
         using reference = T&;
 
-        Iterator(Node* node = nullptr) : NodePtr(node) {}
+        Iterator(Node* Node = nullptr) : NodePtr(Node) {}
 
         reference operator*() const { return NodePtr->data; }
         pointer operator->() const { return &NodePtr->data; }
 
         Iterator& operator++() { NodePtr = NodePtr->next; return *this; }
-        Iterator operator++(int) { Iterator tmp = *this; ++(*this); return tmp; }
+        Iterator operator++(int) { Iterator Tmp = *this; ++(*this); return Tmp; }
         Iterator& operator--() { NodePtr = NodePtr->prev; return *this; }
-        Iterator operator--(int) { Iterator tmp = *this; --(*this); return tmp; }
+        Iterator operator--(int) { Iterator Tmp = *this; --(*this); return Tmp; }
 
-        bool operator==(const Iterator& other) const { return NodePtr == other.NodePtr; }
-        bool operator!=(const Iterator& other) const { return NodePtr != other.NodePtr; }
+        bool operator==(const Iterator& Other) const { return NodePtr == Other.NodePtr; }
+        bool operator!=(const Iterator& Other) const { return NodePtr != Other.NodePtr; }
     };
 
     class ConstIterator {
@@ -57,19 +57,19 @@ public:
         using pointer = const T*;
         using reference = const T&;
 
-        ConstIterator(const Node* node = nullptr) : NodePtr(node) {}
-        ConstIterator(const Iterator& it) : NodePtr(it.NodePtr) {}
+        ConstIterator(const Node* Node = nullptr) : NodePtr(Node) {}
+        ConstIterator(const Iterator& It) : NodePtr(It.NodePtr) {}
 
         reference operator*() const { return NodePtr->data; }
         pointer operator->() const { return &NodePtr->data; }
 
         ConstIterator& operator++() { NodePtr = NodePtr->next; return *this; }
-        ConstIterator operator++(int) { ConstIterator tmp = *this; ++(*this); return tmp; }
+        ConstIterator operator++(int) { ConstIterator Tmp = *this; ++(*this); return Tmp; }
         ConstIterator& operator--() { NodePtr = NodePtr->prev; return *this; }
-        ConstIterator operator--(int) { ConstIterator tmp = *this; --(*this); return tmp; }
+        ConstIterator operator--(int) { ConstIterator Tmp = *this; --(*this); return Tmp; }
 
-        bool operator==(const ConstIterator& other) const { return NodePtr == other.NodePtr; }
-        bool operator!=(const ConstIterator& other) const { return NodePtr != other.NodePtr; }
+        bool operator==(const ConstIterator& Other) const { return NodePtr == Other.NodePtr; }
+        bool operator!=(const ConstIterator& Other) const { return NodePtr != Other.NodePtr; }
     };
 
     using iterator = Iterator;
@@ -78,23 +78,23 @@ public:
     // ========== 构造与析构 ==========
     GList() : Head(nullptr), Tail(nullptr), Size(0) {}
 
-    GList(std::initializer_list<T> init) : GList() {
-        for (const auto& value : init) {
-            Append(value);
+    GList(std::initializer_list<T> Init) : GList() {
+        for (const auto& Value : Init) {
+            Append(Value);
         }
     }
 
-    GList(const GList& other) : GList() {
-        for (const auto& value : other) {
-            Append(value);
+    GList(const GList& Other) : GList() {
+        for (const auto& Value : Other) {
+            Append(Value);
         }
     }
 
-    GList(GList&& other) noexcept
-        : Head(other.Head), Tail(other.Tail), Size(other.Size) {
-        other.Head = nullptr;
-        other.Tail = nullptr;
-        other.Size = 0;
+    GList(GList&& Other) noexcept
+        : Head(Other.Head), Tail(Other.Tail), Size(Other.Size) {
+        Other.Head = nullptr;
+        Other.Tail = nullptr;
+        Other.Size = 0;
     }
 
     ~GList() {
@@ -102,31 +102,31 @@ public:
     }
 
     // ========== 赋值操作 ==========
-    GList& operator=(const GList& other) {
-        if (this != &other) {
-            GList tmp(other);
-            Swap(tmp);
+    GList& operator=(const GList& Other) {
+        if (this != &Other) {
+            GList Tmp(Other);
+            Swap(Tmp);
         }
         return *this;
     }
 
-    GList& operator=(GList&& other) noexcept {
-        if (this != &other) {
+    GList& operator=(GList&& Other) noexcept {
+        if (this != &Other) {
             Clear();
-            Head = other.Head;
-            Tail = other.Tail;
-            Size = other.Size;
-            other.Head = nullptr;
-            other.Tail = nullptr;
-            other.Size = 0;
+            Head = Other.Head;
+            Tail = Other.Tail;
+            Size = Other.Size;
+            Other.Head = nullptr;
+            Other.Tail = nullptr;
+            Other.Size = 0;
         }
         return *this;
     }
 
-    GList& operator=(std::initializer_list<T> init) {
+    GList& operator=(std::initializer_list<T> Init) {
         Clear();
-        for (const auto& value : init) {
-            Append(value);
+        for (const auto& Value : Init) {
+            Append(Value);
         }
         return *this;
     }
@@ -154,203 +154,203 @@ public:
         return Tail->data;
     }
 
-    T& At(size_t index) {
-        if (index >= Size) throw std::out_of_range("GList::At(): index out of range");
-        return NodeAt(index)->data;
+    T& At(size_t Index) {
+        if (Index >= Size) throw std::out_of_range("GList::At(): index out of range");
+        return NodeAt(Index)->data;
     }
-    const T& At(size_t index) const {
-        if (index >= Size) throw std::out_of_range("GList::At(): index out of range");
-        return NodeAt(index)->data;
+    const T& At(size_t Index) const {
+        if (Index >= Size) throw std::out_of_range("GList::At(): index out of range");
+        return NodeAt(Index)->data;
     }
 
-    T& operator[](size_t index) { return NodeAt(index)->data; }
-    const T& operator[](size_t index) const { return NodeAt(index)->data; }
+    T& operator[](size_t Index) { return NodeAt(Index)->data; }
+    const T& operator[](size_t Index) const { return NodeAt(Index)->data; }
 
     // ========== 添加元素 ==========
-    void Append(const T& value) {
-        Node* newNode = new Node(value);
+    void Append(const T& Value) {
+        Node* NewNode = new Node(Value);
         if (IsEmpty()) {
-            Head = Tail = newNode;
+            Head = Tail = NewNode;
         } else {
-            Tail->next = newNode;
-            newNode->prev = Tail;
-            Tail = newNode;
+            Tail->next = NewNode;
+            NewNode->prev = Tail;
+            Tail = NewNode;
         }
         ++Size;
     }
 
-    void Append(T&& value) {
-        Node* newNode = new Node(std::move(value));
+    void Append(T&& Value) {
+        Node* NewNode = new Node(std::move(Value));
         if (IsEmpty()) {
-            Head = Tail = newNode;
+            Head = Tail = NewNode;
         } else {
-            Tail->next = newNode;
-            newNode->prev = Tail;
-            Tail = newNode;
+            Tail->next = NewNode;
+            NewNode->prev = Tail;
+            Tail = NewNode;
         }
         ++Size;
     }
 
-    void Prepend(const T& value) {
-        Node* newNode = new Node(value);
+    void Prepend(const T& Value) {
+        Node* NewNode = new Node(Value);
         if (IsEmpty()) {
-            Head = Tail = newNode;
+            Head = Tail = NewNode;
         } else {
-            newNode->next = Head;
-            Head->prev = newNode;
-            Head = newNode;
+            NewNode->next = Head;
+            Head->prev = NewNode;
+            Head = NewNode;
         }
         ++Size;
     }
 
-    void Prepend(T&& value) {
-        Node* newNode = new Node(std::move(value));
+    void Prepend(T&& Value) {
+        Node* NewNode = new Node(std::move(Value));
         if (IsEmpty()) {
-            Head = Tail = newNode;
+            Head = Tail = NewNode;
         } else {
-            newNode->next = Head;
-            Head->prev = newNode;
-            Head = newNode;
+            NewNode->next = Head;
+            Head->prev = NewNode;
+            Head = NewNode;
         }
         ++Size;
     }
 
-    void Insert(size_t index, const T& value) {
-        if (index == 0) {
-            Prepend(value);
-        } else if (index >= Size) {
-            Append(value);
+    void Insert(size_t Index, const T& Value) {
+        if (Index == 0) {
+            Prepend(Value);
+        } else if (Index >= Size) {
+            Append(Value);
         } else {
-            Node* current = NodeAt(index);
-            Node* newNode = new Node(value);
-            newNode->prev = current->prev;
-            newNode->next = current;
-            current->prev->next = newNode;
-            current->prev = newNode;
+            Node* Current = NodeAt(Index);
+            Node* NewNode = new Node(Value);
+            NewNode->prev = Current->prev;
+            NewNode->next = Current;
+            Current->prev->next = NewNode;
+            Current->prev = NewNode;
             ++Size;
         }
     }
 
-    void Insert(size_t index, T&& value) {
-        if (index == 0) {
-            Prepend(std::move(value));
-        } else if (index >= Size) {
-            Append(std::move(value));
+    void Insert(size_t Index, T&& Value) {
+        if (Index == 0) {
+            Prepend(std::move(Value));
+        } else if (Index >= Size) {
+            Append(std::move(Value));
         } else {
-            Node* current = NodeAt(index);
-            Node* newNode = new Node(std::move(value));
-            newNode->prev = current->prev;
-            newNode->next = current;
-            current->prev->next = newNode;
-            current->prev = newNode;
+            Node* Current = NodeAt(Index);
+            Node* NewNode = new Node(std::move(Value));
+            NewNode->prev = Current->prev;
+            NewNode->next = Current;
+            Current->prev->next = NewNode;
+            Current->prev = NewNode;
             ++Size;
         }
     }
 
     // ========== 移除元素 ==========
-    void RemoveAt(size_t index) {
-        if (index >= Size) throw std::out_of_range("GList::RemoveAt(): index out of range");
-        Node* nodeToRemove = NodeAt(index);
-        RemoveNode(nodeToRemove);
-        delete nodeToRemove;
+    void RemoveAt(size_t Index) {
+        if (Index >= Size) throw std::out_of_range("GList::RemoveAt(): index out of range");
+        Node* NodeToRemove = NodeAt(Index);
+        RemoveNode(NodeToRemove);
+        delete NodeToRemove;
     }
 
-    bool Remove(const T& value) {
-        Node* node = FindNode(value);
-        if (!node) return false;
-        RemoveNode(node);
-        delete node;
+    bool Remove(const T& Value) {
+        Node* FoundNode = FindNode(Value);
+        if (!FoundNode) return false;
+        RemoveNode(FoundNode);
+        delete FoundNode;
         return true;
     }
 
-    size_t RemoveAll(const T& value) {
-        size_t count = 0;
-        Node* current = Head;
-        while (current) {
-            Node* next = current->next;
-            if (current->data == value) {
-                RemoveNode(current);
-                delete current;
-                ++count;
+    size_t RemoveAll(const T& Value) {
+        size_t Count = 0;
+        Node* Current = Head;
+        while (Current) {
+            Node* Next = Current->next;
+            if (Current->data == Value) {
+                RemoveNode(Current);
+                delete Current;
+                ++Count;
             }
-            current = next;
+            Current = Next;
         }
-        return count;
+        return Count;
     }
 
     void Clear() {
-        Node* current = Head;
-        while (current) {
-            Node* next = current->next;
-            delete current;
-            current = next;
+        Node* Current = Head;
+        while (Current) {
+            Node* Next = Current->next;
+            delete Current;
+            Current = Next;
         }
         Head = Tail = nullptr;
         Size = 0;
     }
 
     // ========== 查找 ==========
-    Iterator Find(const T& value) {
-        Node* current = Head;
-        while (current) {
-            if (current->data == value) {
-                return Iterator(current);
+    Iterator Find(const T& Value) {
+        Node* Current = Head;
+        while (Current) {
+            if (Current->data == Value) {
+                return Iterator(Current);
             }
-            current = current->next;
+            Current = Current->next;
         }
         return End();
     }
 
-    ConstIterator Find(const T& value) const {
-        Node* current = Head;
-        while (current) {
-            if (current->data == value) {
-                return ConstIterator(current);
+    ConstIterator Find(const T& Value) const {
+        Node* Current = Head;
+        while (Current) {
+            if (Current->data == Value) {
+                return ConstIterator(Current);
             }
-            current = current->next;
+            Current = Current->next;
         }
         return End();
     }
 
-    size_t IndexOf(const T& value, size_t from = 0) const {
-        Node* current = NodeAt(from);
-        size_t index = from;
-        while (current) {
-            if (current->data == value) {
-                return index;
+    size_t IndexOf(const T& Value, size_t From = 0) const {
+        Node* Current = NodeAt(From);
+        size_t Index = From;
+        while (Current) {
+            if (Current->data == Value) {
+                return Index;
             }
-            current = current->next;
-            ++index;
+            Current = Current->next;
+            ++Index;
         }
         return static_cast<size_t>(-1);
     }
 
-    bool Contains(const T& value) const {
-        return Find(value) != End();
+    bool Contains(const T& Value) const {
+        return Find(Value) != End();
     }
 
     // ========== 其他操作 ==========
     void Reverse() {
-        Node* current = Head;
-        Node* temp = nullptr;
+        Node* Current = Head;
+        Node* Temp = nullptr;
         Tail = Head;
 
-        while (current) {
-            temp = current->prev;
-            current->prev = current->next;
-            current->next = temp;
-            current = current->prev;
+        while (Current) {
+            Temp = Current->prev;
+            Current->prev = Current->next;
+            Current->next = Temp;
+            Current = Current->prev;
         }
 
-        if (temp) {
-            Head = temp->prev;
+        if (Temp) {
+            Head = Temp->prev;
         }
     }
 
-    void Swap(GList& other) noexcept {
-        std::swap(Head, other.Head);
-        std::swap(Tail, other.Tail);
-        std::swap(Size, other.Size);
+    void Swap(GList& Other) noexcept {
+        std::swap(Head, Other.Head);
+        std::swap(Tail, Other.Tail);
+        std::swap(Size, Other.Size);
     }
 
     // ========== 迭代器支持 ==========
@@ -369,47 +369,47 @@ public:
     ConstIterator cend() const { return ConstIterator(nullptr); }
 
 private:
-    Node* NodeAt(size_t index) const {
-        if (index >= Size) return nullptr;
+    Node* NodeAt(size_t Index) const {
+        if (Index >= Size) return nullptr;
 
         // 选择从头还是从尾遍历（优化）
-        if (index < Size / 2) {
-            Node* current = Head;
-            for (size_t i = 0; i < index; ++i) {
-                current = current->next;
+        if (Index < Size / 2) {
+            Node* Current = Head;
+            for (size_t i = 0; i < Index; ++i) {
+                Current = Current->next;
             }
-            return current;
+            return Current;
         } else {
-            Node* current = Tail;
-            for (size_t i = Size - 1; i > index; --i) {
-                current = current->prev;
+            Node* Current = Tail;
+            for (size_t i = Size - 1; i > Index; --i) {
+                Current = Current->prev;
             }
-            return current;
+            return Current;
         }
     }
 
-    Node* FindNode(const T& value) const {
-        Node* current = Head;
-        while (current) {
-            if (current->data == value) {
-                return current;
+    Node* FindNode(const T& Value) const {
+        Node* Current = Head;
+        while (Current) {
+            if (Current->data == Value) {
+                return Current;
             }
-            current = current->next;
+            Current = Current->next;
         }
         return nullptr;
     }
 
-    void RemoveNode(Node* node) {
-        if (node->prev) {
-            node->prev->next = node->next;
+    void RemoveNode(Node* NodeToRemove) {
+        if (NodeToRemove->prev) {
+            NodeToRemove->prev->next = NodeToRemove->next;
         } else {
-            Head = node->next;
+            Head = NodeToRemove->next;
         }
 
-        if (node->next) {
-            node->next->prev = node->prev;
+        if (NodeToRemove->next) {
+            NodeToRemove->next->prev = NodeToRemove->prev;
         } else {
-            Tail = node->prev;
+            Tail = NodeToRemove->prev;
         }
 
         --Size;
